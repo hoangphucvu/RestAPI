53867cb2508b4f860416d6054eb2c88d17f3e5d3
Ngo Hung Phuc <ngohungphuc95@gmail.com>
2016-06-16 20:39:25 +0700

    Init commit

---
+++ KMS.TwitterClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KMS.TwitterClient.MvcApplication" Language="C#" %>
---
+++ KMS.TwitterClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KMS.TwitterClient.Startup))]
+namespace KMS.TwitterClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
